b991522eef28db248b279cca2a028db1d99e7e10
xdarkphoneix <rajsaurav589@gmail.com>
2024-05-08 15:04:46 +0530

    third commit

--- APP.js
+++ APP.js
@@ -1,5 +1,9 @@
 let searchbtn=document.getElementById("searchbtn");
 let inputsearch=document.getElementById("input_search");
+let box1=document.getElementById("box1");
+let box2=document.getElementById("box2");
+let box3=document.getElementById("box3");
+let box4=document.getElementById("box4");
 let moviename;
 
 
@@ -10,3 +14,81 @@ searchbtn.addEventListener("click",()=>{
     window.location.href="newpage.html";
    
 })
+
+
+
+
+box1.addEventListener("click",()=>{
+    moviename="ranneeti";
+  console.log(moviename);
+  localStorage.setItem("moviename",moviename);
+  window.location.href="newpage.html";
+   
+
+})
+
+box2.addEventListener("click",()=>{
+    moviename="lootera";
+  console.log(moviename);
+  localStorage.setItem("moviename",moviename);
+  window.location.href="newpage.html";
+   
+
+})
+
+box3.addEventListener("click",()=>{
+    moviename="monkey man";
+  console.log(moviename);
+  localStorage.setItem("moviename",moviename);
+  window.location.href="newpage.html";
+   
+
+})
+
+box4.addEventListener("click",()=>{
+    moviename="white rose";
+  console.log(moviename);
+  localStorage.setItem("moviename",moviename);
+  window.location.href="newpage.html";
+   
+
+})
+
+box5.addEventListener("click",()=>{
+    moviename="bajirao mastani";
+  console.log(moviename);
+  localStorage.setItem("moviename",moviename);
+  window.location.href="newpage.html";
+   
+
+})
+box6.addEventListener("click",()=>{
+    moviename="shaitaan";
+  console.log(moviename);
+  localStorage.setItem("moviename",moviename);
+  window.location.href="newpage.html";
+   
+
+})
+
+box7.addEventListener("click",()=>{
+
+    moviename="anabeelle setupathi";
+  console.log(moviename);
+  localStorage.setItem("moviename",moviename);
+  window.location.href="newpage.html";
+   
+
+})
+
+box8.addEventListener("click",()=>{
+    moviename="heeramandi";
+  console.log(moviename);
+  localStorage.setItem("moviename",moviename);
+  window.location.href="newpage.html";
+   
+
+})
+
+
+
--- newpage.js
+++ newpage.js
@@ -6,29 +6,46 @@ let title=document.getElementById("title");
    let genre=document.getElementById("genre");
    let movie_img=document.getElementById("image-container");
    let myImage=document.getElementById("myImage");
+   let releasedate=document.getElementById("released-date")
+   let awards=document.getElementById("awards");
+   let country=document.getElementById("country");
+   let director=document.getElementById("director");
+   let language=document.getElementById("language");
+   let year=document.getElementById("year");
+   let runtime=document.getElementById("runtime");
+   let imdbrating=document.getElementById("imdbrating");
+   let rating=document.getElementById("rating");
+   let writer=document.getElementById("writer");
+
+ 
 let url;
 const movie_name=localStorage.getItem("moviename");
 console.log(movie_name);
-
 const getdata= async(moviename)=>{
   
     const url=`https://www.omdbapi.com/?i=tt3896198&apikey=de71a43&t=${moviename}`;
    let response=await fetch(url);
    //console.log(response);
    let data= await response.json();
-   title.innerText=data.Title;
-  actor.innerText=data.Actors;
-  genre.innerText=data.Genre;
-myImage.src=data.Poster;
-  console.log(data.Poster);
-   plot.innerText=data.Plot;
-  console.log(plot.innerText);
-   
+   title.innerText="Title: " +data.Title;
+  actor.innerText="Actos: " +data.Actors;
+  genre.innerText="Genre: "+ data.Genre;
+  myImage.src=data.Poster;
+   plot.innerText="Plot: "+data.Plot;
+   awards.innerText="Awards: "+data.Awards;
+   country.innerText="Country :"+data.Country;
+   director.innerText="Director :"+data.Director;
+   year.innerText="Year :"+data.Year;
+   runtime.innerText="Runtime :"+data.Runtime;
+   imdbrating.innerText="imdbRating :"+data.imdbRating;
+   writer.innerText="Writer : "+data.Writer;
+   language.innerText="Language :"+data.Language;
 
    console.log(data);
-   };
-   
-   getdata(movie_name);
 
-   
+
+  
+   };
+
+   getdata(movie_name);
 
